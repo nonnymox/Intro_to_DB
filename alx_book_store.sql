bbfe74a4669cb82c0c4725a0e53c4d70521ac5ac
Emmanuel Chukwunonso Ighavbota <emmanuelighavbota@gmail.com>
2024-10-19 21:40:33 +0100

    Created the bookstore database

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,1 +1,46 @@
+-- Create the database for the online bookstore and select the database to use for subsequent operations
+CREATE DATABASE alx_book_store;
+USE alx_book_store;
+
+-- Create the Authors table to store author information
+CREATE TABLE Authors (
+    author_id INT AUTO_INCREMENT PRIMARY KEY,
+    author_name VARCHAR(215) NOT NULL
+);
+
+-- Create the Books table to store book information
+CREATE TABLE Books (
+    book_id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(130) NOT NULL,
+    author_id INT,
+    price DOUBLE NOT NULL,
+    publication_date DATE,
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
+);
+
+-- Create the Customers table to store customer information
+CREATE TABLE Customers (
+    customer_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_name VARCHAR(215) NOT NULL,
+    email VARCHAR(215) NOT NULL,
+    address TEXT
+);
+
+-- Create the Orders table to store order information
+CREATE TABLE Orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,
+    customer_id INT,
+    order_date DATE,
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+);
+
+-- Create the Order_Details table to store the details of books in each order
+CREATE TABLE Order_Details (
+    orderdetailid INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    book_id INT,
+    quantity DOUBLE NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
+);
 
