a4239d4a248325e388de7431a45d6eb0af4ffd7b
Emmanuel Chukwunonso Ighavbota <emmanuelighavbota@gmail.com>
2024-10-20 03:56:20 +0100

    Usinng python to connnect to a db

---
+++ MySQLServer.py
@@ -0,0 +1,32 @@
+import mysql.connector
+from mysql.connector import Error
+
+
+def create_database():
+    try:
+        # Establish connection to MySQL server
+        connection = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            password="swap22...")
+
+        if connection.is_connected():
+            cursor = connection.cursor()
+
+            # Create the database if it doesn't exist
+            cursor.execute("CREATE DATABASE IF NOT EXISTS alx_book_store")
+            print("Database 'alx_book_store' created successfully!")
+
+    except Error as e:
+        print(f"Error connecting to MySQL: {e}")
+
+    finally:
+        # Ensure the connection is closed
+        if connection.is_connected():
+            cursor.close()
+            connection.close()
+            print("MySQL connection closed.")
+
+
+if __name__ == "__main__":
+    create_database()
